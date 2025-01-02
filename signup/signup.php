d2041332e4c44d0b2344bb7b0943a3a7ac622424
nathanael <me@nattho.com>
2025-01-02 11:39:25 +0000

    set to use values entered

--- signup/signup.php
+++ signup/signup.php
@@ -1,36 +1,36 @@
 <?php
-// // Start the session
-// session_start();
+// Start the session
+session_start();
 
-// // Check if the form is submitted
-// if ($_SERVER["REQUEST_METHOD"] == "POST") {
-//     // Collect and sanitize input data
-//     $username = htmlspecialchars($_POST['username']);
-//     $password = htmlspecialchars($_POST['password']);
+// Check if the form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Collect and sanitize input data
+    $username = htmlspecialchars($_POST['username']);
+    $firstname = htmlspecialchars($_POST['firstname']);
+}
 
-//     // Save info to the database
-//   $_SESSION['username'] = $username;
-//     header("Location: welcome.php");
-//     exit();
-// }
-$servername = "localhost";
-$username = "nathanael";
-$password = '!@F@QfLCu_p2Xhk@rgjv';
-$dbname = "homepage";
+// Save info to the database
+  $servername = "localhost";
+  $susername = "nathanael"; //This var is called susername so php can distinguish between the username for the server and the username entered
+  $password = '!@F@QfLCu_p2Xhk@rgjv';
+  $dbname = "homepage";
 
-try {
-  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $sql = "INSERT INTO `logins` (`isAdmin`, `Username`, `FirstName`)
-  VALUES (0, 'natthopo', 'Natthaphon')";
-  // use exec() because no results are returned
-  $conn->exec($sql);
-  echo "New record created successfully";
-} catch(PDOException $e) {
-  echo $sql . "<br>" . $e->getMessage();
+  try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $susername, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "INSERT INTO `logins` (`Username`, `FirstName`)
+    VALUES ('$username', '$firstname')";
+    // use exec() because no results are returned
+    $conn->exec($sql);
+    echo "Account created successfully";
+  } catch(PDOException $e) {
+    echo $sql . "<br>" . $e->getMessage();
 } 
 
 $conn = null;
+
+exit();
+
 
 ?>
