040af64b3a990df7117e3c928cea400ee282f16d
nathanael <me@nattho.com>
2025-01-06 16:12:14 +0000

    testinput function added

--- signup/signup.php
+++ signup/signup.php
@@ -14,6 +14,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 }
 
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
 // Save info to the database
 
   try {
