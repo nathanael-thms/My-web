b8cd69eb59072384094adc91b61f6fc32ae05bd4
nathanael <me@nattho.com>
2025-01-07 18:05:13 +0000

    username and firstname required

--- signup/index.php
+++ signup/index.php
@@ -4,24 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign Up</title>
+    <link rel="stylesheet" href="./css/css.css">
 </head>
 <body>
     <h1>Sign Up</h1>
     <form method="post">
         <label for="username">Username</label>
-        <input type="text" name="username" id="username" required>
+        <input type="text" name="username" id="username">
+        <span class="error">* <?php echo $nameErr;?></span>
         <br><br>
         <label for="firstname">First Name</label>
         <input type="text" name="firstname" id="firstname" required>
+        <span class="error">* <?php echo $nameErr;?></span>
         <br><br>
         <label for="lastname">Last Name</label>
         <input type="text" name="lastname" id="lastname">
         <br><br>
         <label for="email">Email</label>
-        <input type="email" name="email" id="email">
+        <input type="text" name="email" id="email">
         <br><br>
         <label for="phoneNumber">Phone Number</label>
-        <input type="tel" name="phoneNumber" id="phoneNumber">
+        <input type="text" name="phoneNumber" id="phoneNumber">
         <br><br>
         <button type="submit">Sign Up</button>
     </form>
@@ -29,15 +32,27 @@
 require_once('../boot.php');
 // Start the session
 session_start();
+$nameErr = $emailErr = $genderErr = "";
 
 // Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Collect and sanitize input data
-    $username = test_input($_POST['username']);
-    $firstname = test_input($_POST['firstname']);
-    $lastname = test_input($_POST['lastname']);
-    $email = test_input($_POST['email']);
-    $phoneNumber = test_input($_POST['phoneNumber']);
+    if (empty($_POST["username"])) {
+        $nameErr = "Username is required";
+    } else {
+        $username = test_input($_POST["username"]);
+    }
+
+    if (empty($_POST["firstname"])) {
+        $nameErr = "First name is required";
+    } else {
+        $firstname = test_input($_POST["firstname"]);
+    }
+    // $username = test_input($_POST['username']);
+    // $firstname = test_input($_POST['firstname']);
+    // $lastname = test_input($_POST['lastname']);
+    // $email = test_input($_POST['email']);
+    // $phoneNumber = test_input($_POST['phoneNumber']);
 
 }
 
